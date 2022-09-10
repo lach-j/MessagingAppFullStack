cac582816e730267d0b120a81d4ee7a0565a943c
Lachlan Johnson <lachlanm.johnson@gmail.com>
2022-09-10 19:52:50 +1000

    Add test for groupby

---
+++ MessagingAppFullStack/ClientApp/src/tests/groupBy.test.ts
@@ -0,0 +1,7 @@
+﻿import {groupBy} from "../util/groupBy";
+
+test('groupBy splits items', () => {
+    let elements = ['a', 'b', 'b', 'b', 'a', 'a', 'c', 'b', 'd'];
+    let groupedElements = groupBy(elements, (a, b) => a === b);
+    expect(groupedElements.map(a => a.length)).toEqual([1, 3, 2, 1, 1, 1]);
+})
