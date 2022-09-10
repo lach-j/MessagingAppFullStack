26f242ff3255e660b0efaf1191befd31ee536bfd
Lachlan Johnson <lachlanm.johnson@gmail.com>
2022-09-10 19:02:35 +1000

    Add message component

---
+++ MessagingAppFullStack/ClientApp/src/components/MessageComponent.tsx
@@ -0,0 +1,21 @@
+﻿import {Avatar, Box, Flex, HStack, Stack} from "@chakra-ui/react";
+import {User} from "../models/User";
+
+export interface MessageComponentProps {
+    user: User;
+    content: string;
+    avatarSide?: 'left' | 'right';
+    backgroundColor?: string;
+}
+
+export const MessageComponent = ({user, content, avatarSide = 'left', backgroundColor = 'red.100'}: MessageComponentProps) => {
+    
+    return (
+        <Flex direction={avatarSide === 'left' ? 'row' : 'row-reverse'} alignItems='end' gap={2}>
+            <Avatar name={user.name} src={user.avatar} size='sm' />
+            <Box backgroundColor={backgroundColor} px={4} py={2} rounded={'xl'} verticalAlign='middle' lineHeight='1.8rem'>
+                {content}
+            </Box>
+        </Flex>
+    );
+}
---
+++ MessagingAppFullStack/ClientApp/src/stories/MessageComponent.stories.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {ComponentStory, ComponentMeta} from '@storybook/react';
+
+import {MessageComponent} from "../components/MessageComponent";
+
+export default {
+    title: 'Messaging/MessageComponent',
+    component: MessageComponent,
+} as ComponentMeta<typeof MessageComponent>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof MessageComponent> = (args) => <MessageComponent {...args}/>;
+
+export const Default = Template.bind({});
+Default.args = {user: {id: 0, name: "Lachlan Johnson", avatar: "https://source.unsplash.com/random/300×300"}, avatarSide: 'left', content: 'Hello my name is Lachlan', backgroundColor: 'blue.100'}
