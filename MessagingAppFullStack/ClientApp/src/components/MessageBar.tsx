e216b0aba9b566c699156bb8abfc705832b58d5f
Lachlan Johnson <lachlanm.johnson@gmail.com>
2022-09-10 12:55:29 +1000

    Implement basic messaging bar

---
+++ MessagingAppFullStack/ClientApp/src/components/MessageBar.tsx
@@ -0,0 +1,30 @@
+﻿import { HStack, IconButton, Input} from "@chakra-ui/react";
+import React from "react";
+import {BiPaperPlane} from "react-icons/all";
+
+interface MessageBarProps {
+    onSend: (messageContent: string) => void;
+}
+
+export const MessageBar = ({ onSend }: MessageBarProps) => {
+    
+    const [messageContent, setMessageContent] = React.useState<string>("");
+    
+    const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setMessageContent(e.target.value);
+    }
+    
+    const handleSendMessage = () => {
+        if (!messageContent) return;
+        
+        onSend(messageContent);
+        setMessageContent("");
+    }
+    
+    return (
+      <HStack>
+          <Input value={messageContent} onChange={handleInput} />
+          <IconButton colorScheme={'blue'} icon={<BiPaperPlane />} aria-label={'Send message'} onClick={handleSendMessage} />
+      </HStack>  
+    );
+}
---
+++ MessagingAppFullStack/ClientApp/src/stories/MessageBar.stories.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import {MessageBar} from "../components/MessageBar";
+
+export default {
+  title: 'Messaging/MessageBar',
+  component: MessageBar,
+  argTypes: {
+    onSend: { action: 'clicked' }
+  }
+} as ComponentMeta<typeof MessageBar>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof MessageBar> = (args) => <MessageBar {...args} />;
+
+export const Primary = Template.bind({});
