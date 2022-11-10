261a31b4cb456ffc29836fbc8acfd7f793a022a1
Lachlan Johnson <lachlanm.johnson@gmail.com>
2022-11-10 22:17:28 +1100

    Add swagger

--- MessagingAppFullStack/Program.cs
+++ MessagingAppFullStack/Program.cs
@@ -1,6 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddSwaggerGen();
 
 builder.Services.AddControllersWithViews();
 
@@ -12,6 +14,11 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+else
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
@@ -21,8 +28,9 @@ app.UseRouting();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller}/{action=Index}/{id?}");
+app.MapControllers();
 
 app.MapFallbackToFile("index.html");
-;
+
 
 app.Run();
