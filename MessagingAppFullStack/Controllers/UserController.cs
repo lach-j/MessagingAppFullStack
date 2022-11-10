7f9a848b9a9d1184641c17e735f2285d65ae535c
Lachlan Johnson <lachlanm.johnson@gmail.com>
2022-11-10 22:33:35 +1100

    Add controller for creating test users

---
+++ MessagingAppFullStack/Controllers/UserController.cs
@@ -0,0 +1,60 @@
+﻿using MessagingAppFullStack.Domain.Models;
+using MessagingAppFullStack.Requests;
+using MessagingAppFullStack.Security;
+using MessagingAppFullStack.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MessagingAppFullStack.Controllers;
+
+[ApiController]
+[Authorize]
+[Route("[controller]")]
+public class UserController : ControllerBase
+{
+    private readonly IUserService _userService;
+
+    public UserController(IUserService userService)
+    {
+        _userService = userService;
+    }
+
+    [HttpPost]
+    [AllowAnonymous]
+    public async Task<ActionResult<User>> CreateUser(NewUserRequest userRequest)
+    {
+        if (userRequest.ConfirmPassword != userRequest.Password)
+            return BadRequest(new
+            {
+                Errors = new Dictionary<string, string[]>()
+                {
+                    {nameof(NewUserRequest.ConfirmPassword), new[] {"Passwords must match."}}
+                }
+            });
+
+        var user = new User
+        {
+            Username = userRequest.Email,
+            Password = BCrypt.Net.BCrypt.HashPassword(userRequest.Password)
+        };
+
+        var createdUser = await _userService.CreateUserAsync(user);
+        if (createdUser is null)
+            return Conflict(new { Message = $"{nameof(User)} with email '{userRequest.Email}' already exists." });
+        return Ok(createdUser);
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<User>>> GetUsers()
+    {
+        return await _userService.GetAllUsersAsync();
+    }
+
+    
+    [HttpGet]
+    [Route("{userId:long}")]
+    public async Task<ActionResult> GetUserById([FromRoute] long userId)
+    {
+        return Ok(await _userService.GetUserByIdAsync(userId));
+    }
+}
