ab2ba4cdaf551fe6f215bed537491e464b3883dc
Lachlan Johnson <lachlanm.johnson@gmail.com>
2022-11-10 22:35:17 +1100

    Add AuthController for username/password auth and generate JWT

---
+++ MessagingAppFullStack/Requests/TokenRequest.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MessagingAppFullStack.Requests;
+
+public class TokenRequest
+{
+    [Required]
+    public string Username { get; set; }
+    [Required]
+    public string Password { get; set; }
+}
