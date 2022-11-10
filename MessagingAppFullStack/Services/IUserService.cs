a7bb708508cf7791952f169db6eaffdac1010cc9
Lachlan Johnson <lachlanm.johnson@gmail.com>
2022-11-10 22:25:49 +1100

    Create user service for User CRUD operations

---
+++ MessagingAppFullStack/Services/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using MessagingAppFullStack.Domain.Models;
+
+namespace MessagingAppFullStack.Services;
+
+public interface IUserService
+{
+    public Task<User?> GetUserByIdAsync(long id);
+    public Task<User?> GetUserByEmailAsync(string email);
+    public Task<List<User>> GetUsersByIdsAsync(IEnumerable<long> ids);
+
+    public Task<User?> CreateUserAsync(User user);
+    public Task<List<User>> GetAllUsersAsync();
+}
